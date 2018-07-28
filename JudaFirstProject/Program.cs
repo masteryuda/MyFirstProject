71ef8700cfbb69101bc5698c373a4d1c64743794
Haludi <igalmer@gmail.com>
2018-07-28 18:28:05 +0300

    String extension

--- JudaFirstProject/Program.cs
+++ JudaFirstProject/Program.cs
@@ -9,4 +9,25 @@ namespace JudaFirstProject
             Console.WriteLine("Hello World!");
         }
     }
+
+    class StringExtension
+    {
+        public static void Revers(string str)
+        {
+            foreach (char ch in str)
+            {
+                Console.Write(ch);
+            }
+
+        }
+
+        public static void PrintRevers(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                char ch = str[str.Length - i - 1];
+                Console.Write(ch);
+            }
+        }
+    }
 }
