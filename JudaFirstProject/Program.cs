cf9f0270a5959c18da86101a10846664940403eb
Haludi <igalmer@gmail.com>
2018-08-04 11:45:06 +0300

    Clear only sub files folder

--- JudaFirstProject/Program.cs
+++ JudaFirstProject/Program.cs
@@ -1,13 +1,90 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 namespace JudaFirstProject
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            File.WriteAllLines("C:\\Users\\Haludi\\OneDrive\\Documents\\stam.txt")
+            if (args.Length != 1)
+            {
+                return;
+            }
+
+            string Path = args[0];
+
+            if (!Directory.Exists(Path))
+            {
+                return;
+            }
+
+            string[] directories = Directory.GetDirectories(Path);
+            List<string> directoriesToDelete = GetFoldersToDelete(directories);
+
+            if (directoriesToDelete.Count == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine("The following diriectories has been found");
+            Console.WriteLine("__________________________________________");
+
+            for (int i = 0; i < directoriesToDelete.Count; i++)
+            {
+                Console.WriteLine(directoriesToDelete[i]);
+            }
+            Console.WriteLine("Do you want to delete?");
+
+            var answer = Console.ReadLine();
+            if (answer == "Y" || answer == "y" || answer == "YES" || answer == "Yes" || answer == "YeS")
+            {
+                //לולאות או לא להיות
+                for (int i = 0; i < directoriesToDelete.Count; i++)
+                {
+                    Directory.Delete(directoriesToDelete[i], true);
+                }
+            }
+        }
+
+        private static List<string> GetFoldersToDelete(string[] directories)
+        {
+            List<string> directoriesToDelete = new List<string>();
+
+            foreach (string directory in directories)
+            {
+                if (DoesHaveOnllySubtitleFiles(directory))
+                {
+                    directoriesToDelete.Add(directory);
+                }
+            }
+
+            return directoriesToDelete;
+        }
+
+        static string[] subExtensions = new string[]
+        {
+            "sub",
+            "srt",
+            "sbv"
+        };
+
+
+        private static bool DoesHaveOnllySubtitleFiles(string directory)
+        {
+            string[] allFiles = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+
+            int subtitleFileAmount = 0;
+            foreach (string extension in subExtensions)
+            {
+                string[] subFiles = Directory.GetFiles(directory, "*." + extension, SearchOption.AllDirectories);
+                subtitleFileAmount += subFiles.Length;
+            }
+
+            return allFiles.Length == subtitleFileAmount;
         }
     }
 
